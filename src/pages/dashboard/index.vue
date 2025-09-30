890652660e61eae45a50ede3c5c82a1a2c440b18
sicily621 <zhangm_2014@163.com>
2025-09-30 18:35:04 +0800

    首页

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,13 +1,71 @@
-<script lang="ts" setup>
-import { useUserStore } from "@/pinia/stores/user"
-import Admin from "./components/Admin.vue"
-import Editor from "./components/Editor.vue"
-
-const userStore = useUserStore()
-
-const isAdmin = userStore.roles.includes("admin")
-</script>
-
 <template>
-  <component :is="isAdmin ? Admin : Editor" />
+  <div class="app-container flex flex-col h-full p-4">
+    <div class="flex-1 el-card p-4 m-b-2">
+      <Line
+        title="年度销售趋势"
+        :x-axis-data="xAxisData"
+        :series-data="seriesData"
+        :animation="true"
+        :show-area="false"
+        @click="handleChartClick"
+      />
+    </div>
+    <div class="flex-1 el-card p-4 m-b-2">
+      <Line
+        title="年度销售趋势"
+        :x-axis-data="xAxisData"
+        :series-data="seriesData"
+        :animation="true"
+        :show-area="false"
+        @click="handleChartClick"
+      />
+    </div>
+    <div class="flex-1 el-card p-4">
+      <Bar
+        title="各产品季度销量对比"
+        :x-axis-data="xAxisData"
+        :series-data="seriesData"
+        :animation="true"
+        :is-stacked="false"
+        :show-label="true"
+        @click="handleChartClick"
+      />
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import Line from "@@/components/echarts/line.vue";
+import Bar from "@@/components/echarts/bar.vue";
+const xAxisData = [
+  "1月",
+  "2月",
+  "3月",
+  "4月",
+  "5月",
+  "6月",
+  "7月",
+  "8月",
+  "9月",
+  "10月",
+  "11月",
+  "12月",
+];
+
+const seriesData = [
+  {
+    name: "产品A",
+    data: [120, 132, 101, 134, 90, 230, 210, 230, 190, 230, 210, 180],
+    color: "#42b983",
+  },
+  {
+    name: "产品B",
+    data: [220, 182, 191, 234, 290, 330, 310, 330, 320, 330, 310, 340],
+    color: "#3598db",
+  },
+];
+
+const handleChartClick = (params: any) => {
+  console.log("点击了图表:", params);
+};
+</script>
